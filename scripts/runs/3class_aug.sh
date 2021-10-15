545c06887a92d53f77fad62aeb79808ee75306a8
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-15 14:07:48 +0200

    adding run bash files to run experiments on cluste

---
+++ scripts/runs/3class_aug.sh
@@ -0,0 +1,3 @@
+source deactivate
+source activate thesis_cluster
+python scripts/train_simple.py --cache_dir ~/.heart_echo --scaling_factor 0.25 --label_type 3class --batch_size 30 --max_epochs 1000 --class_balance_per_epoch --augment
