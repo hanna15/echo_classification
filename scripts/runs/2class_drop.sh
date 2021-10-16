052403f89ad6d1e77237f47082d47a75c2653486
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-16 23:06:29 +0200

    add pip install -e . to all run files

--- scripts/runs/2class_drop.sh
+++ scripts/runs/2class_drop.sh
@@ -1,3 +1,4 @@
 source deactivate
 source activate thesis_cluster2
+pip install -e .
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 08:00 -n 6 "python scripts/train_simple.py --cache_dir ~/.heart_echo --scaling_factor 0.25 --label_type 2class_drop_ambiguous --batch_size 30 --max_epochs 1000"
