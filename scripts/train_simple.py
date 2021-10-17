a26cb0f5f732035da25a4e74396b92a73c7a3f0f
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-17 12:07:23 +0200

    add validation metrics to wanb log dict

--- scripts/train_simple.py
+++ scripts/train_simple.py
@@ -178,7 +178,7 @@ def train(model, train_loader, valid_loader, data_len, valid_len, weights=None, 
                 "train loss": epoch_loss / data_len
             }
             log_dict.update(epoch_metrics)
-            epoch_metrics.update(epoch_valid_metrics)
+            log_dict.update(epoch_valid_metrics)
             wandb.log(log_dict)
         scheduler.step(epoch_valid_loss / valid_len)  # Update learning rate scheduler
         if args.debug:
