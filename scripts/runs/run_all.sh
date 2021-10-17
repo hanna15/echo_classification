e41f0096ab4645a84d6e1d50a752d6b4bca5dbe4
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-17 17:45:32 +0200

    minor

---
+++ scripts/runs/run_all.sh
@@ -0,0 +1,7 @@
+./scripts/runs/2class.sh
+./scripts/runs/2class_aug.sh
+./scripts/runs/2class_pretrained.sh
+./scripts/runs/2class_drop.sh
+./scripts/runs/2class_drop_aug.sh
+./scripts/runs/3class.sh
+./scripts/runs/3class_aug.sh
