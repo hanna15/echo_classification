86412151caab4ad174ffb9403431e7f2812e3df9
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-18 15:19:22 +0200

    reduce lr in all scripts

--- scripts/runs/run_all.sh
+++ scripts/runs/run_all.sh
@@ -1,6 +1,6 @@
 ./scripts/runs/2class.sh
 ./scripts/runs/2class_aug.sh
-./scripts/runs/2class_pretrained.sh
+./scripts/runs/2class_aug_pretrained.sh
 ./scripts/runs/2class_drop.sh
 ./scripts/runs/2class_drop_aug.sh
 ./scripts/runs/3class.sh
