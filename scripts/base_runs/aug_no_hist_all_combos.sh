174ed003e1bbadc45cb28a3b8983994fffe1fa92
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-20 17:06:52 +0200

    fix the run scripts

--- scripts/base_runs/aug_no_hist_all_combos.sh
+++ scripts/base_runs/aug_no_hist_all_combos.sh
@@ -5,30 +5,34 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
+echo "training on fold $k"
 source deactivate
 source activate thesis_cluster2
-bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --augment --k $1"
+bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 "python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --augment --k $1"
 
-declare -a StringArray=("noise", "intensity", "rand_resize", "rotate", "translate")
+echo "finished starting training on all augments"
+
+declare -a StringArray=("noise" "intensity" "rand_resize" "rotate" "translate")
 for val in ${StringArray[@]}; do
+  echo "starting training on only specific augment: ${val}"
   bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 "python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --${val}"
-
+done
+exit 0
+bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
+	"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --noise --intensity --rand_resize --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --noise --intensity --rand_resize --rotate --translate"
+	"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --intensity --rand_resize --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --intensity --rand_resize --rotate --translate"
+	"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --rand_resize --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --rand_resize --rotate --translate"
+	"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --noise --rand_resize --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --noise --rand_resize --rotate --translate"
+	"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --noise --intensity --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --noise --intensity --rotate --translate"
+	"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --rotate --translate"
 
-bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --rotate --translate"
