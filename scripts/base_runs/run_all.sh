97f952226783482f8d0de17244b5d3421b3a30aa
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-20 15:34:05 +0200

    missing script prefic

--- scripts/base_runs/run_all.sh
+++ scripts/base_runs/run_all.sh
@@ -4,7 +4,7 @@
 # It then runs all the baseline experiments for this fold
 
 echo "Running script for fold no: $1"
-./base_runs/no_aug.sh $1
-./base_runs/hist_only.sh $1
-./base_runs/aug_no_hist_all_combos.sh $1
-./base_runs/aug_hist_all_combos.sh $1
+./scripts/base_runs/no_aug.sh $1
+./scripts/base_runs/hist_only.sh $1
+./scripts/base_runs/aug_no_hist_all_combos.sh $1
+./scripts/base_runs/aug_hist_all_combos.sh $1
