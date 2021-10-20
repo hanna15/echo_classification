910cb098dc567cb4d3fa2f62b1afc211c8483c72
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-20 10:24:43 +0200

    adding run scripts for new augment

---
+++ scripts/2class_drop_runs/run_all.sh
@@ -0,0 +1,6 @@
+./scripts/2class_drop_runs/augment.sh
+./scripts/2class_drop_runs/augment_bal.sh
+./scripts/2class_drop_runs/augment_bal_hist.sh
+./scripts/2class_drop_runs/augment_hist_eq.sh
+./scripts/2class_drop_runs/no_augment.sh
+./scripts/2class_drop_runs/no_augment_hist_eq.sh
