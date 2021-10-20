90cb555b850872eb91ea09500c8f01d0bb92f775
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-20 16:24:20 +0200

    fix formatting of new run script

--- scripts/base_runs/aug_hist_all_combos.sh
+++ scripts/base_runs/aug_hist_all_combos.sh
@@ -3,28 +3,27 @@
 source deactivate
 source activate thesis_cluster2
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --augment --hist'
+"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --augment --hist"
 
 declare -a StringArray=("noise", "intensity", "rand_resize", "rotate", "translate")
 for val in ${StringArray[@]}; do
-  bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-  'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --${val}'
+  bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 "python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --${val}"
 
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --noise --intensity --rand_resize --rotate --translate'
+"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --noise --intensity --rand_resize --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --intensity --rand_resize --rotate --translate'
+"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --intensity --rand_resize --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --rand_resize --rotate --translate'
+"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --rand_resize --rotate --translate""
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --noise --rand_resize --rotate --translate'
+"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --noise --rand_resize --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --noise --intensity --rotate --translate'
+"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --noise --intensity --rotate --translate"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --rotate --translate'
+"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --hist --rotate --translate"
--- scripts/base_runs/hist_only.sh
+++ scripts/base_runs/hist_only.sh
@@ -2,5 +2,4 @@
 
 source deactivate
 source activate thesis_cluster2
-bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-'python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --hist_eq --k $1'
+bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 "python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --hist_eq --k $1"
