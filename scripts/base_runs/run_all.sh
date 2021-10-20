c830c189f76a9f1c5b8f12f5368d5e5936093575
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-20 16:29:54 +0200

    add a reminder to new run scripts to include argument

--- scripts/base_runs/run_all.sh
+++ scripts/base_runs/run_all.sh
@@ -3,6 +3,10 @@
 # This bash script expects one argument, k=current fold.
 # It then runs all the baseline experiments for this fold
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Please provide an argument for k (current fold)'
+    exit 0
+fi
 echo "Running script for fold no: $1"
 ./scripts/base_runs/no_aug.sh $1
 ./scripts/base_runs/hist_only.sh $1
