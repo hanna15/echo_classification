088ed5d758a8fbe4a5a8de0e7606febc1b120639
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-20 11:50:50 +0200

    fix run files

--- scripts/2class_drop_runs_2/run_all.sh
+++ scripts/2class_drop_runs_2/run_all.sh
@@ -1,6 +1,6 @@
-./scripts/2class_drop_runs/augment.sh
-./scripts/2class_drop_runs/augment_bal.sh
-./scripts/2class_drop_runs/augment_bal_hist.sh
-./scripts/2class_drop_runs/augment_hist_eq.sh
-./scripts/2class_drop_runs/no_augment.sh
-./scripts/2class_drop_runs/no_augment_hist_eq.sh
+./scripts/2class_drop_runs_2/augment.sh
+./scripts/2class_drop_runs_2/augment_bal.sh
+./scripts/2class_drop_runs_2/augment_bal_hist.sh
+./scripts/2class_drop_runs_2/augment_hist_eq.sh
+./scripts/2class_drop_runs_2/no_augment.sh
+./scripts/2class_drop_runs_2/no_augment_hist_eq.sh
