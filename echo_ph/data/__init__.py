1d8f11f9426be6b2dc3c06a0bd74c3cb76912651
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-10-22 11:41:55 +0200

    .

--- echo_ph/data/__init__.py
+++ echo_ph/data/__init__.py
@@ -1,3 +1,3 @@
 from .echo_dataset import EchoDataset
 from .segmentation import SegmentationAnalyser
-from .ph_labels import label_map_2class, label_map_3class
+from .ph_labels import label_map_2class, label_map_3class, long_label_type_to_short
