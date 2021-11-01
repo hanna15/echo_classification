02ab0abfca22217f9d978e89b3bb9a6757365599
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-11-01 11:21:54 +0100

    new run file

---
+++ scripts/run_test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source deactivate
+source activate thesis_cluster2
+
+for fold in {0..9}; do
+  echo "Starting run for fold ${fold}"
+    bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
+    "python scripts/train_simple.py --max_epochs 100 --max_p 95 --res_dir results_test --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo  --k 10 --fold ${fold} --augment --hist --class_balance_per_epoch --optimizer adam --pretrained"
+    bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
+    "python scripts/train_simple.py --max_epochs 100 --max_p 95 --res_dir results_test --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo  --k 10 --fold ${fold} --augment --hist --class_balance_per_epoch --optimizer adamw --pretrained"
+    bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
+    "python scripts/train_simple.py --max_epochs 100 --max_p 95 --aug_type 4 --res_dir results_test --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo  --k 10 --fold ${fold} --augment --hist --class_balance_per_epoch --optimizer adam --pretrained"
+    bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
+    "python scripts/train_simple.py --max_epochs 100 --max_p 95 --aug_type 4 results_test --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo  --k 10 --fold ${fold} --augment --hist --class_balance_per_epoch --optimizer adamw --pretrained"
+    bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
+    "python scripts/train_simple.py --max_epochs 300 --max_p 95 --res_dir results_test --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo  --k 10 --fold ${fold} --augment --hist --class_balance_per_epoch --optimizer adam --pretrained"
