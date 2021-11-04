caa6145fcc7f6cd7928040a6c37b39b97a7e57f8
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-11-04 11:06:31 +0100

    minor changes to cluster run files

--- scripts/run_best.sh
+++ scripts/run_best.sh
@@ -3,17 +3,14 @@
 source deactivate
 source activate thesis_cluster2
 
-  for fold in {0..9}; do
+for fold in {0..9}; do
     echo "Starting run for fold ${fold}"
     bsub -R "rusage[mem=2048, ngpus_excl_p=1]" -W 04:00 -n 6 \
-      "python scripts/train_simple.py --max_epochs 300 --max_p 95 --batch_size 64 --wd 0.0001 --res_dir results_best --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo --k 10 --fold ${fold} --hist --class_balance_per_epoch --augment --aug_type 3 --optimizer adamw --pretrained"
-    bsub -R "rusage[mem=2048, ngpus_excl_p=1]" -W 04:00 -n 6 \
-      "python scripts/train_simple.py --max_epochs 300 --max_p 95 --batch_size 64 --wd 0.0001 --res_dir results_wd --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo --k 10 --fold ${fold} --hist --class_balance_per_epoch --optimizer adamw --pretrained"
-    bsub -R "rusage[mem=2048, ngpus_excl_p=1]" -W 04:00 -n 6 \
-      "python scripts/train_simple.py --max_epochs 300 --max_p 95 --batch_size 64 --wd 0.0001 --res_dir results_wd --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo --k 10 --fold ${fold} --hist --class_balance_per_epoch --augment --aug_type 4 --optimizer adamw --pretrained"
-    bsub -R "rusage[mem=2048, ngpus_excl_p=1]" -W 04:00 -n 6 \
-      "python scripts/train_simple.py --max_epochs 300 --max_p 95 --batch_size 64 --wd 0.0001 --res_dir results_wd --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo --k 10 --fold ${fold} --hist --class_balance_per_epoch --augment --aug_type 2 --optimizer adamw --pretrained"
+      "python scripts/train_simple.py --run_id crop_c_random10_corr --min_expansion --max_epochs 300 --max_p 95 --batch_size 64 --res_dir results_crop --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo --k 10 --fold ${fold} --hist --class_balance_per_epoch --augment --aug_type 3 --optimizer adam --pretrained"
+    #bsub -R "rusage[mem=2048, ngpus_excl_p=1]" -W 04:00 -n 6 \
+    #  "python scripts/train_simple.py --max_epochs 300 --max_p 95 --batch_size 64 --res_dir results_best --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo --k 10 --fold ${fold} --hist --class_balance_per_epoch --optimizer adamw --pretrained"
+    #bsub -R "rusage[mem=2048, ngpus_excl_p=1]" -W 04:00 -n 6 \
+    #  "python scripts/train_simple.py --run_id crop_corners --max_epochs 300 --max_p 95 --batch_size 64 --res_dir results_crop  --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo --k 10 --fold ${fold} --hist --class_balance_per_epoch --augment --aug_type 4 --optimizer adam --pretrained"
+    #bsub -R "rusage[mem=2048, ngpus_excl_p=1]" -W 04:00 -n 6 \
+    #  "python scripts/train_simple.py --max_epochs 300 --max_p 95 --batch_size 64 --wd 0.0001 --res_dir results_best --eval_metrics video-b-accuracy/valid --cache_dir ~/.heart_echo --k 10 --fold ${fold} --hist --class_balance_per_epoch --augment --aug_type 2 --optimizer adamw --pretrained"
   done
-done
-
-resnet_adamw_lt_2d.k10.lr_0.001.batch_64.wd_0.0001.me_300_pre_bal_hist_p95.0
--- scripts/run_resnet_simple.sh
+++ scripts/run_resnet_simple.sh
@@ -12,5 +12,5 @@ source activate thesis_cluster2
 echo "Starting a run for fold $1, with dropout value $2"
 
 bsub -R "rusage[mem=4096, ngpus_excl_p=1]" -W 04:00 -n 6 \
-	"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --augment --hist --class_balance_per_epoch --model res_simple --optimizer adamw --dropout $2"
+	"python scripts/train_simple.py --cache_dir ~/.heart_echo --label_type 2class_drop_ambiguous --k $1 --augment --hist --class_balance_per_epoch --model res_simple --dropout $2 --log_freq 1 --max_epochs 300 --optimizer adamw"
 
