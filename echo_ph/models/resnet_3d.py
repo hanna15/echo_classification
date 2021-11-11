b83117702a1bf555cc4c1b1b5ce14ade64f421dc
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2021-11-11 18:23:07 +0100

    add clip len and period possibility for temporal data, as well as new augmentation

--- echo_ph/models/resnet_3d.py
+++ echo_ph/models/resnet_3d.py
@@ -1,8 +1,9 @@
 import torch
+from torchvision import models
 import os
 
 
-def get_resnet3d(num_classes=2, pretrained=True):
+def get_resnet3d_50(num_classes=2, pretrained=True):
     download_model_path = os.path.expanduser('~/.cache/torch/hub/facebookresearch_pytorchvideo_main')
     model = torch.hub.load(download_model_path, 'slow_r50', source='local', pretrained=pretrained)
     in_channels = 1  # Grayscale
@@ -10,4 +11,21 @@ def get_resnet3d(num_classes=2, pretrained=True):
                                            bias=False)
     num_ftrs = model.blocks[5].proj.in_features
     model.blocks[5].proj = torch.nn.Linear(num_ftrs, num_classes)
+    return model
+
+
+def get_resnet3d_18(num_classes=2, model_name='r2plus1d_18', pretrained=True):
+    """
+
+    :param num_classes: 2 or 3
+    :param model_name: One of r2plus1d_18, mc3_18, r3d_18
+    :param pretrained: True or False
+    :return:
+    """
+    model = models.video.__dict__[model_name](pretrained=pretrained)
+    in_channels = 1
+    model.stem[0] = torch.nn.Conv3d(in_channels, 45, kernel_size=(1, 7, 7), stride=(1, 2, 2), padding=(0, 3, 3),
+                                    bias=False)
+    model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+    # model.fc.bias.data[0] = 55.6
     return model
