190b88a415fd1573e6be24d1e29d7919c9ab9e17
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2022-01-11 10:53:15 +0100

    adding old minor changes to video saver

--- echo_ph/visual/video_saver.py
+++ echo_ph/visual/video_saver.py
@@ -3,17 +3,18 @@ import os
 
 
 class VideoSaver():
-    def __init__(self, video_id, grad_cam_frames, out_dir='vis_videos', max_frames=50, fps=10):
-        self.video_id = str(video_id)
+    def __init__(self, video_title, grad_cam_frames, out_dir='vis_videos', max_frames=50, fps=10):
+        self.video_title = str(video_title)
         self.grad_cam_frames = grad_cam_frames
         self.max_frames = max_frames
         self.fps = fps
+        print(self.fps)
         self.out_dir = out_dir
         os.makedirs(out_dir, exist_ok=True)
 
     def save_video(self):
         size = (self.grad_cam_frames[0].shape[0], self.grad_cam_frames[0].shape[1])
-        out_path = os.path.join(self.out_dir, self.video_id + '.avi')
+        out_path = os.path.join(self.out_dir, self.video_title + '.avi')
         result = cv2.VideoWriter(out_path, cv2.VideoWriter_fourcc(*'MJPG'), self.fps, size)
         frames = self.grad_cam_frames[0:self.max_frames]  # E.g. only first 50 frames, or approx first 5 heart-beats
         for frame in frames:
