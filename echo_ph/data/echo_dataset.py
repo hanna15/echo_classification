d6768533bb026a1d6c986e12ae9b279e2e909d85
Hanna Ragnarsdottir <hragnarsd@ethz.ch>
2022-01-28 20:58:33 +0100

    fix temporal for multi-view

--- echo_ph/data/echo_dataset.py
+++ echo_ph/data/echo_dataset.py
@@ -341,9 +341,9 @@ class EchoDataset(Dataset):
         sample_name = self.sample_names[idx]
         frame_per_view = self.frames[idx]
         for view, frames in frame_per_view.items():
-            if not len(np.shape(frames)) > 2:  # If it has not been already modified (due to a multi-processing glitch)
-                if self.temporal:
-                    frames = list(frames)
+            if self.temporal:
+                frames = list(frames)
+            if (not self.temporal and len(np.shape(frames)) <= 2) or (self.temporal and len(np.shape(frames[0])) <= 2):  # If it has not been already modified (due to a multi-processing glitch)
                 s = (frames, sample_name.split('_')[0] + '_' + view)
                 frames = self.transform(s)
             frame_per_view[view] = frames  # over-write with transformed frames
